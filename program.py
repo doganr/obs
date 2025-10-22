e93b806dd65b410bd6bbcc53065ac1d6c57f3a59
Ramazan Özgür DOĞAN <dogan@outlook.com>
2025-10-22 14:54:49 +0300

    Liste olmadan ogrenci listesi olusturma

--- program.py
+++ program.py
@@ -16,12 +16,20 @@ if __name__ == '__main__':
     print(len(baslik))
 
     print('-' * 100)
-    print('|',baslik,' '*(94-(len(baslik)+1)),'|x|')
+    print(f'| {baslik:<94} |x|')
     print('-' * 100)
     
-    ogrenci_adi = input('Ogrenci adini giriniz:')
-    ogrenci_soyadi = input('Ogrenci soyadini giriniz:')
-    ogrenci_numara = input('Ogrenci numarasini giriniz:')
+    ogrenci1_adi = input('1. Ogrenci adini giriniz:')
+    ogrenci1_soyadi = input('1. Ogrenci soyadini giriniz:')
+    ogrenci1_numara = input('1. Ogrenci numarasini giriniz:')
+
+    ogrenci2_adi = input('2. Ogrenci adini giriniz:')
+    ogrenci2_soyadi = input('2. Ogrenci soyadini giriniz:')
+    ogrenci2_numara = input('2. Ogrenci numarasini giriniz:')
+
+    ogrenci3_adi = input('3. Ogrenci adini giriniz:')
+    ogrenci3_soyadi = input('3. Ogrenci soyadini giriniz:')
+    ogrenci3_numara = input('3. Ogrenci numarasini giriniz:')
 
     '''
     | Isim                        | Soyisim                                       | Numara             |
@@ -31,13 +39,9 @@ if __name__ == '__main__':
     '''
 
     print('-' * 100)
-    print('|','Isim',' '*(94-(len(baslik)+1)),'|x|')
+    print(f'|{" "*9}| {"Isim":<35} | {"Soyisim":<25} | {"Numara":<20} |')
     print('-' * 100)
-
-
-
-    # print('|',' '*96,'|')
-    # print('|',' '*96,'|')
-    # print('|',' '*96,'|')
-    # print('|',' '*96,'|')
-    # print('-' * 100)
+    print(f'| {1:>7} | {ogrenci1_adi:<35} | {ogrenci1_soyadi:<25} | {ogrenci1_numara:<20} |')
+    print(f'| {2:>7} | {ogrenci2_adi:<35} | {ogrenci2_soyadi:<25} | {ogrenci2_numara:<20} |')
+    print(f'| {3:>7} | {ogrenci3_adi:<35} | {ogrenci3_soyadi:<25} | {ogrenci3_numara:<20} |')
+    
