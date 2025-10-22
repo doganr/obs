87e3da69eb63c5d55bfbadd1e65c28af3db2ac00
Ramazan Özgür DOĞAN <dogan@outlook.com>
2025-10-22 13:31:14 +0300

    OBS uygulama repomuz olusturuldu!

---
+++ program.py
@@ -0,0 +1,12 @@
+'''
+    yazar: dogan
+    email: dogan@trabzon.edu.tr
+    
+    aciklama: Python programlama dilini
+    bir ogrenci bilgi sistemi senaryosu ile
+    ogreten bir repo (anaprogram dosyasi)
+'''
+
+
+if __name__ == '__main__':
+    print('Ogrenci bilgi sistemine hos geldiniz:')
