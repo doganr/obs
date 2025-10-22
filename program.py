e8e59b4d0bb703b818f83d51b50a49368577994b
Ramazan Özgür DOĞAN <dogan@outlook.com>
2025-10-22 14:32:52 +0300

    string format kullanmadan zorluklar....

--- program.py
+++ program.py
@@ -9,4 +9,35 @@
 
 
 if __name__ == '__main__':
-    print('Ogrenci bilgi sistemine hos geldiniz:')
+
+    baslik = 'Ogrenci Bilgi Sistemi (v1)'
+
+    print(baslik)
+    print(len(baslik))
+
+    print('-' * 100)
+    print('|',baslik,' '*(94-(len(baslik)+1)),'|x|')
+    print('-' * 100)
+    
+    ogrenci_adi = input('Ogrenci adini giriniz:')
+    ogrenci_soyadi = input('Ogrenci soyadini giriniz:')
+    ogrenci_numara = input('Ogrenci numarasini giriniz:')
+
+    '''
+    | Isim                        | Soyisim                                       | Numara             |
+    ----------------------------------------------------------------------------------------------------
+    | ramazan                     | dogan                                         | 12345              |
+    | hulya                       | dogan                                         | 12346              |
+    '''
+
+    print('-' * 100)
+    print('|','Isim',' '*(94-(len(baslik)+1)),'|x|')
+    print('-' * 100)
+
+
+
+    # print('|',' '*96,'|')
+    # print('|',' '*96,'|')
+    # print('|',' '*96,'|')
+    # print('|',' '*96,'|')
+    # print('-' * 100)
