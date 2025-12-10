fc628cf6689f61d4e7d1d39cb4897f46bb1bb293
Ramazan Özgür DOĞAN <dogan@outlook.com>
2025-12-10 15:17:13 +0300

    dosya okuma devam

---
+++ ornek.sql
@@ -0,0 +1,19 @@
+merhaba
+#klshkljsdjkf
+
+INSERT INTO bolumler (bolum_id, ad, bina) VALUES
+    (1, 'Bilgisayar Mühendisliği', 'A Blok'),
+    (2, 'Yapay Zeka Mühendisliği', 'A Blok'),
+    (3, 'Yazılım Mühendisliği', 'A Blok'),
+    (4, 'Elektrik-Elektronik Mühendisliği', 'B Blok'),
+    (5, 'Endüstri Mühendisliği', 'B Blok'),
+    (6, 'Makine Mühendisliği', 'C Blok'),
+    (7, 'İnşaat Mühendisliği', 'C Blok'),
+    (8, 'Matematik', 'D Blok'),
+    (9, 'Fizik', 'D Blok'),
+    (10, 'İstatistik', 'D Blok');
+
+
+deneme
+ali
+veli
